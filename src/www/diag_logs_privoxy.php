89e1a1679f13099b65ff60bd09fc96419a9e6d53
Damien Vargas <zorille@free.FR>
2019-10-13 17:25:29 +0100

    first commit for os-privoxy

---
+++ src/www/diag_logs_privoxy.php
@@ -0,0 +1,11 @@
+<?php
+
+$logfile = '/var/log/privoxy/logfile';
+$logclog = false;
+
+$service_hook = 'privoxy';
+
+//Date and type fields number
+$logsplit = 3;
+
+require_once 'diag_logs_template.inc';
